818102b37ebdcdfe6dfc2249515319f1b1433ad0
Lee Jung Ho <117253154+Jarabro@users.noreply.github.com>
2025-03-25 09:06:52 +0900

    Add files via upload

--- dog_classifier.py
+++ dog_classifier.py
@@ -1,6 +1,6 @@
 import numpy as np
-import random
-
+from matplotlib import pyplot as plt
+from sklearn.metrics import accuracy_score
 
 dachshund_length = np.random.randint(60, 80, size= 200) # 닥스훈트 길이 200개 생성
 dachshund_height = np.random.randint(20, 40, size= 200) # 닥스훈트 높이 200개 생성
@@ -21,13 +21,13 @@ new_samoyd_length_data = np.random.normal(samoyd_length_mean, 10.0, 200) #사모
 new_samoyd_height_data = np.random.normal(samoyd_height_mean, 10.0, 200) #시모예드의 평균 길이에 좌우 10.0씩 늘려서 랜덤 200개 생성
 
 new_dachshund_data = np.column_stack((new_dachshund_length_data, new_dachshund_height_data)) #닥스훈트의 길이 높이 2차원배열로 합치기
-new_samoyd_data = np.column_stack((new_dachshund_length_data, new_samoyd_height_data)) #사모예드의 길이 높이 2차원배열로 합치기
+new_samoyd_data = np.column_stack((new_samoyd_length_data, new_samoyd_height_data)) #사모예드의 길이 높이 2차원배열로 합치기
 
 dachshund_label = np.zeros(len(new_dachshund_data)) #닥스훈트는 0으로표시
 samoyd_label = np.ones(len(new_samoyd_data)) #사모예드는 1으로표시
 
-unknown_dog_length = np.random.randint(1, 100, size=5) #개 5마리의 길이 5개 생성
-unknown_dog_height = np.random.randint(1, 60, size=5) #개 5마리의 높이 5개 생성
+unknown_dog_length = np.random.normal((dachshund_length_mean + samoyd_length_mean) / 2, 10.0, size=5) #개 5마리의 길이 5개 생성
+unknown_dog_height = np.random.normal((dachshund_height_mean + samoyd_height_mean) / 2, 10.0, size=5)#개 5마리의 높이 5개 생성
 
 new_unknown_dog_data = np.column_stack((unknown_dog_length, unknown_dog_height)) # 높이와 길이 배열 합치기
 
@@ -35,6 +35,13 @@ dogs = np.concat((new_dachshund_data, new_samoyd_data), axis=0)
 labels = np.concat((dachshund_label, samoyd_label), axis=0)
 dog_classes = {0:"닥스훈트", 1:"사모예드"}
 
+plt.scatter(new_dachshund_length_data,new_dachshund_height_data, c= 'c', marker='o')
+plt.scatter(new_samoyd_length_data,new_samoyd_height_data, c= 'b', marker='*')
+plt.scatter(unknown_dog_length,unknown_dog_height, c= 'r', marker='p')
+plt.xlabel("Height")
+plt.ylabel("Length")
+plt.legend(["dachshund","samoyd","Unknown_Dog"],loc="upper right")
+plt.show()
 
 from sklearn.neighbors import KNeighborsClassifier
 k = 3
@@ -45,3 +52,8 @@ y_predict = knn.predict(new_unknown_dog_data)
 print(y_predict)
 print(f"이 강아지는 : {dog_classes[y_predict[0]]},{dog_classes[y_predict[1]]},"
       f"{dog_classes[y_predict[2]]},{dog_classes[y_predict[3]]},{dog_classes[y_predict[4]]}")
+print(dogs.shape)
+print(labels.shape)
+
+print(f'훈련 정확도 : {knn.score(X=dogs, y=labels)}')
+print(f'예측 정확도 : {accuracy_score(labels, knn.predict(dogs))}')
